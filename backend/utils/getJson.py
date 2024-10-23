1e636022c996868f5fdb4f699be55baec0271327
UttU28 <utsavmaan28@gmail.com>
2024-10-23 13:32:30 -0500

    JSK

--- backend/utils/getJson.py
+++ backend/utils/getJson.py
@@ -11,29 +11,35 @@ async def checkDuplicate(data):
 async def getMeJsonData(htmlContent):
     li_element = BeautifulSoup(htmlContent, 'html.parser')
     
-    position = None
-    company_name = None
-    location = None
+    companyPosition = None
+    companyName = None
+    companyLocation = None
     
-    position_tag = li_element.find('div', class_='display-flex align-items-center mr1 t-bold')
-    if position_tag:
-        position = position_tag.get_text(strip=True)
-    
-    company_info_tag = li_element.find('span', class_='t-14 t-normal')
-    if company_info_tag:
-        company_info = company_info_tag.get_text(strip=True)
-        # print("company_info", company_info)
-        company_info_parts = company_info.split(' · ')
-        company_name = company_info_parts[0] if company_info_parts else None
+    mainTags = li_element.find_all('div', class_='display-flex flex-wrap align-items-center full-height')
+    if len(mainTags) > 1:
+        companyName = mainTags[0].find('div', class_='display-flex').get_text(strip=True)
+        companyPosition = mainTags[1].find('div', class_='display-flex').get_text(strip=True)
+        companyLocation = li_element.find_all('span', class_='pvs-entity__caption-wrapper')[0].get_text(strip=True)
+    else:
+        positionTag = li_element.find('div', class_='display-flex align-items-center mr1 t-bold')
+        if positionTag:
+            companyPosition = positionTag.get_text(strip=True)
+        
+        company_info_tag = li_element.find('span', class_='t-14 t-normal')
+        if company_info_tag:
+            company_info = company_info_tag.get_text(strip=True)
+            print("company_info", company_info)
+            company_info_parts = company_info.split(' · ')
+            companyName = company_info_parts[0] if company_info_parts else None
 
-    location_tags = li_element.find_all('span', class_='t-14 t-normal t-black--light')
-    if len(location_tags) > 1:
-        location = location_tags[1].get_text(strip=True)
+        location_tags = li_element.find_all('span', class_='t-14 t-normal t-black--light')
+        if len(location_tags) > 1:
+            companyLocation = location_tags[1].get_text(strip=True)
 
     jobData = {
-        "companyName": await checkDuplicate(company_name),
-        "companyPosition": await checkDuplicate(position),
-        "companyLocation": await checkDuplicate(location)
+        "companyName": await checkDuplicate(companyName),
+        "companyPosition": await checkDuplicate(companyPosition),
+        "companyLocation": await checkDuplicate(companyLocation)
     }
     
     return jobData
@@ -50,14 +56,14 @@ async def findClosestMatch(inputString, data):
     normalized_input = await normalizeString(inputString)
     
     for index, entry in enumerate(data):
-        normalized_company_name = await normalizeString(entry['companyName'])
-        if normalized_company_name in normalized_input or normalized_input in normalized_company_name:
+        normalized_companyName = await normalizeString(entry['companyName'])
+        if normalized_companyName in normalized_input or normalized_input in normalized_companyName:
             return data[index]
         input_components = normalized_input.split()
-        if all(comp in normalized_company_name for comp in input_components):
+        if all(comp in normalized_companyName for comp in input_components):
             return data[index]
         
-        if normalized_input.replace(" ", "") in normalized_company_name.replace(" ", ""):
+        if normalized_input.replace(" ", "") in normalized_companyName.replace(" ", ""):
             return data[index]
         
     return None
--- backend/utils/workingSelenium.py
+++ backend/utils/workingSelenium.py
@@ -53,46 +53,34 @@ async def scrapeDataFrom(thisDriver, companyName, lastName, firstName):
                     liElements[0].click()
 
                     # THIS IS AFTER CLICKING ON THE PERSON
-                    print(1)
                     sleep(2)
-                    print(2)
 
                     currentUrl = thisDriver.current_url
                     htmlContent = thisDriver.page_source
-                    # CHAING THIDJHVLKJS DBSJKH GJSHG
-                    sectionList = thisDriver.find_elements(By.CSS_SELECTOR, "section.artdeco-card.pv-profile-card.break-words")
+                    soup = BeautifulSoup(htmlContent, 'html.parser')
+                    sectionList = soup.select("section.artdeco-card.pv-profile-card.break-words")
                     print(f"Number of sections found: {len(sectionList)}")
-                    print(sectionList[1].get_attribute('innerHTML'))
+
                     for section in sectionList:
-                        try:
-                            experienceDiv = WebDriverWait(section, 10).until(
-                                EC.presence_of_element_located((By.CSS_SELECTOR, "div.pv-profile-card__anchor"))
-                            )
-                            print(experienceDiv.get_attribute('id'))
-                            if experienceDiv.get_attribute('id') == 'experience':
-                                # experienceDiv = section.find_element(By.ID, 'experience')
-                                print(len(experienceDiv), "this is the lenght")
-                                print(3)
-                                jobDataList = []
-                                if experienceDiv:
-                                    ulElements = section.find_elements(By.TAG_NAME, 'ul')
-                                    listElements = ulElements[0].find_elements(By.TAG_NAME, 'li')
+                        experienceDiv = section.select_one("div.pv-profile-card__anchor")
 
-                                    for li in listElements:
-                                        liClass = li.get_attribute("class")
-                                        if 'artdeco-list__item' in liClass:
-                                            jobData = await getMeJsonData(li.get_attribute('innerHTML'))
-                                            jobDataList.append(jobData)
+                        if experienceDiv and experienceDiv.get('id') == 'experience':
+                            print(experienceDiv.get('id'))
+                            jobDataList = []
+                            ulElements = section.find_all('ul')
+                            if ulElements:
+                                listElements = ulElements[0].find_all('li')
 
-                                    print(companyName, lastName, firstName)
-                                    thisData = await findClosestMatch(companyName, jobDataList)
-                                    thisData['currentUrl'] = currentUrl
-                                    print("thisData", thisData)
-                                    return thisData
-                            return {'currentUrl': currentUrl}
-                        except Exception:
-                            print("Error in finding experience div")
-                            continue
+                                for li in listElements:
+                                    liClass = li.get('class', [])
+                                    if 'artdeco-list__item' in liClass:
+                                        jobData = await getMeJsonData(str(li))
+                                        jobDataList.append(jobData)
+
+                                thisData = await findClosestMatch(companyName, jobDataList)
+                                thisData['currentUrl'] = currentUrl
+                                return thisData
+
                     return {'currentUrl': currentUrl}
         except:
             continue  
