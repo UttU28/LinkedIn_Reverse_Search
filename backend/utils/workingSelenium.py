962001005c2fce6e78b66e33373dad8de974c1bc
Karyakarta <utsavmaan28@gmail.com>
2024-11-08 17:18:38 -0600

    Perfect

--- backend/utils/workingSelenium.py
+++ backend/utils/workingSelenium.py
@@ -12,7 +12,7 @@ try: from utils.getJson import getMeJsonData, findClosestMatch
 except: from getJson import getMeJsonData, findClosestMatch
 
 # PTATH_TILL_PROJECT = 'C:/Users/utsav/OneDrive/Desktop/LinkedIn_Reverse_Search/'
-PTATH_TILL_PROJECT = "C:/Users/UtsavChaudhary/OneDrive - EDGE196/Desktop/LinkedIn_Reverse_Search/"
+PTATH_TILL_PROJECT = "C:/Users/UtsavChaudhary/Desktop/LinkedIn_Reverse_Search/"
 chromeDriverPath = f'{PTATH_TILL_PROJECT}backend/chromeDriver/chromedriver.exe'
 
 options = Options()
